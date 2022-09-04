3c351c9903ec6629b362a6da39427083ad19aa62
Quadrizz <mackquadrizz@gmail.com>
2022-09-04 15:21:31 +0100

    bucket boy

--- src/App.js
+++ src/App.js
@@ -14,15 +14,12 @@ function App() {
   const [Loading1, setLoading1] = useState(false)
   const [Loading2, setLoading2] = useState(false)
   const validateTweet = new RegExp(/((https?):\/\/)?(www.)?twitter\.com(\/@?(\w){1,15})\/status\/[0-9]{19}\?/);
-  const token = "AAAAAAAAAAAAAAAAAAAAAH%2FpgQEAAAAAOeUVljoK7fFyhzljKOQmKRG9MHU%3DehQiyRP2VCiW6UyMOSiw6lDir3lMzrZb6145JdGmuGPHYRP0nv";
   const twit = "This is a test twit"
   const height = window.innerHeight;
 
 
   
   useEffect(()=>{
-    console.log(validTweet)
-    console.log(tweet)
     let slash = 0;
     let user_id = '';
     let tweet_id = '';
@@ -86,6 +83,10 @@ function App() {
     window.open(`https://twitter.com/intent/tweet?text=${twit}`,'_blank')
   }
 
+  const handleFollowClick = ()=>{
+    window.open('https://twitter.com/Bucketboynft?s=20&t=0Gl2xIonLpbchxKCVJ7xDQ','_blank')
+  }
+
   const handleFollow = ()=>{
     setLoading2(true)
     fetch('http://34.125.117.167:3001/getfollowers',{
@@ -95,7 +96,7 @@ function App() {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        id: '882932059022385152'
+        id: '1536787378861383680'
       })
     }).then(response => response.json())
     .then((data) => {
@@ -106,10 +107,10 @@ function App() {
         }
         if(data.data[i].username === username){
           setLoading2(false)
-          window.open("https:fb.com", '_blank')
+          window.open("https://discord.gg/SVybaRPV", '_blank')
+          setError2(false)
         }
         else{
-          console.log("Hello")
           continue
         }
       }
@@ -119,11 +120,12 @@ function App() {
   return (
     <div className="App" style={{height:height + 'px'}}>
       <div className='Images'>
-        <img className='first' src='https://cdn.discordapp.com/attachments/1013804888385404979/1013805249330430072/bucket_head4.png'/>
-        <img className='second' src='https://cdn.discordapp.com/attachments/1013804888385404979/1013805249665966130/Bucket_water_mark_1.png'/>
+        <img className='first' src='https://cdn.discordapp.com/attachments/1013804888385404979/1013805249330430072/bucket_head4.png' alt='BucketBoy Text'/>
+        <img className='second' src='https://cdn.discordapp.com/attachments/1013804888385404979/1013805249665966130/Bucket_water_mark_1.png' alt='BucketBoy Bucket'/>
       </div>
       <div className='Forms'>
         <h2>Follow these steps to join the discord server.</h2>
+        <h3>The below steps are important to authenticate users in our community. Please complete them all to verify your role within our discord.</h3>
         <div className={step === 1 ? 'form1 show' : 'form1'}>
           <button disabled={step === 1 ? false : true} className='tweetButton' onClick={handleTweetClick}>Tweet</button>
           <input disabled={step === 1 ? false : true} placeholder='Enter tweet link'  name='TweetLink' onChange={handleChange}></input>
@@ -132,8 +134,8 @@ function App() {
         </div>
         <div className={step === 2 ? 'form2 show' : 'form2'}>
             <h3>Follow this account</h3>
-            <button disabled={butValid}>Follow</button>
-            <button disabled={butValid} onClick={handleFollow}>{Loading1? "↻" : "Click Here to Join"}</button>
+            <button disabled={butValid} onClick={handleFollowClick}>Follow</button>
+            <button disabled={butValid} onClick={handleFollow}>{Loading2? "↻" : "Click Here to Join"}</button>
             <p className={Error2? "error show1" : "error"}>Ensure you followed the account</p>
         </div>
       </div>
